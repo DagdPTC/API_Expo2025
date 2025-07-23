597ed1092d5d9d604677ecbea597d384b7e16cbd
Daniel Alvarado <20210133@ricaldone.edu.sv>
2025-07-22 19:13:52 -0600

    Nuevas secuencias

--- src/main/java/OrderlyAPI/Expo2025/Repositories/MetodoPago/MetodoPagoRepository.java
+++
@@ -1,9 +0,0 @@
-package OrderlyAPI.Expo2025.Repositories.MetodoPago;
-
-import OrderlyAPI.Expo2025.Entities.MetodoPago.MetodoPagoEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MetodoPagoRepository extends JpaRepository<MetodoPagoEntity, Long> {
-}
