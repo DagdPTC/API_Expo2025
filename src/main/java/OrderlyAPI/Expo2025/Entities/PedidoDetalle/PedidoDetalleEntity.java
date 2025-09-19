3b854566746cf5c9dd68758c2a638b1a685c1ef5
Darío <20240167@ricaldone.edu.sv>
2025-09-19 16:15:27 -0600

    En proceso

--- src/main/java/OrderlyAPI/Expo2025/Entities/PedidoDetalle/PedidoDetalleEntity.java
+++ src/main/java/OrderlyAPI/Expo2025/Entities/PedidoDetalle/PedidoDetalleEntity.java
@@ -1,4 +1,13 @@
 package OrderlyAPI.Expo2025.Entities.PedidoDetalle;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+@Entity
+@Table(name = "PEDIDODETALLE")
 public class PedidoDetalleEntity {
+
 }
